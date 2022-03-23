8d0e48a343ee0f4b196e085660d42221c3a06016
AndreiAndrewDrew <andreiandrewg@gmail.com>
2022-03-23 15:56:02 +0200

    modificat Cerctests.java

--- homeWork1/src/test/java/qa/homeWork/CercTests.java
+++ homeWork1/src/test/java/qa/homeWork/CercTests.java
@@ -8,11 +8,9 @@ public class CercTests {
 
   @Test
   public void testAriaCerc() {
-    //Square s = new Square(5);
+
     Cerc c = new Cerc(5);
-    //assert s.ariapatrat()==25;
-    //Assert.assertEquals(s.ariapatrat(),25.0);
     Assert.assertEquals(c.AriaCerc(),157.0);
-    //pentru aratarea variantei gresite
+
   }
 }
