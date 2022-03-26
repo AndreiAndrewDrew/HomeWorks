6429b0c49fdf87a1cdffb1222b3925c9f3b4469f
Badura Andrei <andreiandrewg@gmail.com>
2022-03-26 18:22:41 +0200

    modificari in Equation si EquaionTest

--- homeWork2/src/test/java/qa/homeWork2/appmanager/ApplicationManager.java
+++ homeWork2/src/test/java/qa/homeWork2/appmanager/ApplicationManager.java
@@ -1,23 +1,42 @@
 package qa.homeWork2.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
+
   protected WebDriver driver;
 
   private final StringBuffer verificationErrors = new StringBuffer();
 
-  private GroupHelper groupHelper ;
-  private NavigationHelper navigationHelper ;
+  private GroupHelper groupHelper;
+  private NavigationHelper navigationHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    driver = new FirefoxDriver();
+
+    if (browser == BrowserType.FIREFOX) {
+      driver = new FirefoxDriver();
+    } else if (browser==BrowserType.CHROME){
+      driver = new ChromeDriver();
+    } else if(browser==BrowserType.EDGE){
+      driver = new EdgeDriver();
+    }
+
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/");
 
--- homeWork2/src/test/java/qa/homeWork2/tests/TestBase.java
+++ homeWork2/src/test/java/qa/homeWork2/tests/TestBase.java
@@ -1,12 +1,13 @@
 package qa.homeWork2.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import qa.homeWork2.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeClass(alwaysRun = true)
   public void setUp() {
