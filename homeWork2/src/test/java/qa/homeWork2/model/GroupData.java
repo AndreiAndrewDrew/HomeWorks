14f1b7c5c54c5f2b8e3d4050177747a50125bfda
AndreiAndrewDrew <andreiandrewg@gmail.com>
2022-04-04 16:08:31 +0300

    Compare collection

--- homeWork2/src/test/java/qa/homeWork2/model/GroupData.java
+++ homeWork2/src/test/java/qa/homeWork2/model/GroupData.java
@@ -1,4 +1,10 @@
 package qa.homeWork2.model;
 //am creat un record in loc de clasa
 public record GroupData(String name, String header, String footer) {
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
 }
--- homeWork2/src/test/java/qa/homeWork2/tests/GroupDeletionTests.java
+++ homeWork2/src/test/java/qa/homeWork2/tests/GroupDeletionTests.java
@@ -13,19 +13,26 @@ public class GroupDeletionTests extends TestBase {
     //Delete group
     app.getNavigationHelper().gotoGroupPage();
 
-    if (! app.getGroupHelper().isThereAGroup()){
+    if (!app.getGroupHelper().isThereAGroup()) {
 
       app.getGroupHelper().createGroup(new GroupData("Addnewtest44", null, null));
 
     }
     //int before = app.getGroupHelper().getGroupCount();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectedGroup(before.size()-1);
+    app.getGroupHelper().selectedGroup(before.size() - 1);
     app.getGroupHelper().deleteGroup();
     app.getGroupHelper().returnGroupPage();
     //int after = app.getGroupHelper().getGroupCount();
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() -1);
+
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    /*for (int i = 0; i < after.size(); i++) {
+      Assert.assertEquals(before.get(i),after.get(i));
+    }*/
+    Assert.assertEquals(before,after);
   }
 
 }
