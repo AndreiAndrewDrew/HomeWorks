75c38e8d6e51e6538bf8380b1ef699a2702a23fb
AndreiAndrewDrew <andreiandrewg@gmail.com>
2022-04-18 14:42:25 +0300

    Description of the configuration tests

--- homeWork2/src/test/java/qa/homeWork2/appmanager/ApplicationManager.java
+++ homeWork2/src/test/java/qa/homeWork2/appmanager/ApplicationManager.java
@@ -7,11 +7,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
+
+  private final Properties properties;
 
   protected WebDriver driver;
 
@@ -21,32 +28,34 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
-
-  private String browser;
+  private final String browser;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target","local");
+    properties.load(new FileReader(new File(String
+            .format("src/test/resources/%s.properties",target))));
     //cream conditie de alegere a browserului
-    if (browser.equals(BrowserType.FIREFOX)) {
-      driver = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)){
-      driver = new ChromeDriver();
-    } else if(browser.equals(BrowserType.EDGE)){
-      driver = new EdgeDriver();
+    switch (browser) {
+      case BrowserType.FIREFOX -> driver = new FirefoxDriver();
+      case BrowserType.CHROME -> driver = new ChromeDriver();
+      case BrowserType.EDGE -> driver = new EdgeDriver();
     }
 
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
+    driver.get(properties.getProperty("web.baseUrl"));
 
     groupHelper = new GroupHelper(driver);
     navigationHelper = new NavigationHelper(driver);
     sessionHelper = new SessionHelper(driver);
     contactHelper = new ContactHelper(driver);
 
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin")
+            , properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
--- homeWork2/src/test/java/qa/homeWork2/tests/TestBase.java
+++ homeWork2/src/test/java/qa/homeWork2/tests/TestBase.java
@@ -5,12 +5,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import qa.homeWork2.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
-  public void setUp() throws Exception {app.init();}
+  public void setUp() throws IOException {app.init();}
 
   @AfterSuite
   public void tearDown() {
